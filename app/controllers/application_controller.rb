a07a4964f33ddd876b99a7b2abdccd42db026986
syouko11 <piano19980226Nanami@icloud.com>
2023-06-29 21:25:25 +0900

    環境変数設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :besic_auth
+
+  private
+
+  def besic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
